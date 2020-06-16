c3de560a6ed2e5ef889fdc28894627f9a664b82d
Sam Merrell <merrell.sam@gmail.com>
2020-06-16 10:51:16 -0600

    Update virtual server export template
    Add tests as well.

--- converter/virtualServer.go
+++ converter/virtualServer.go
@@ -1,1 +1,31 @@
 package converter
+
+import (
+	"strings"
+	"text/template"
+
+	"github.com/f5devcentral/go-bigip"
+)
+
+func Export(vs *bigip.VirtualServer) string {
+	vstemplate := `resource "bigip_ltm_virtual_server" "{{.Name}}" {
+	name        = "{{.FullPath}}"
+	description = "{{.Description}}"
+}`
+
+	var builder strings.Builder
+
+	if vs == nil {
+		return ""
+	}
+
+	tmpl, err := template.New("vs").Parse(vstemplate)
+	if err != nil {
+		panic(err)
+	}
+	err = tmpl.Execute(&builder, vs)
+	if err != nil {
+		panic(err)
+	}
+	return builder.String()
+}
--- converter/virtualServer_test.go
+++ converter/virtualServer_test.go
@@ -1,7 +1,30 @@
 package converter
 
-import "testing"
+import (
+	"testing"
 
-func TestFoo(t *testing.T) {
-	// t.Errorf("Uh oh")
+	"github.com/f5devcentral/go-bigip"
+)
+
+func TestNilVirtualServerReturnsEmptyString(t *testing.T) {
+	vs := Export(nil)
+	if len(vs) > 0 {
+		t.Errorf("\nExpected empty string: got\n'%v'", vs)
+	}
+}
+
+func TestVirtualServerExport(t *testing.T) {
+	template := `resource "bigip_ltm_virtual_server" "myvirtual_server" {
+	name        = "/Common/myvirtual_server"
+	description = "This is a virtual server description"
+}`
+	server := &bigip.VirtualServer{
+		Name:        "myvirtual_server",
+		FullPath:    "/Common/myvirtual_server",
+		Description: "This is a virtual server description",
+	}
+	vs := Export(server)
+	if vs != template {
+		t.Errorf("Expected: \n%v\nGot:\n%v", template, vs)
+	}
 }
